4a72adada4b6ae7ec6e92f4f5d28df6aca47dc4c
ivan <15101535064@163.com>
2018-10-24 23:12:40 +0800

    添加springboot模版项目

---
+++ springboot/src/main/java/com/zd/learn/springboot/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.zd.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
